f14bbc2b92fc08145e8383e5e4a3c6c260384a59
Aznaur <aznaur.khapchaev@gmail.com>
2019-10-24 19:17:21 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,6 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var csso = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore")
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
